1a5797d66e5a549ce8038e8574558ebbaca81d80
Gardimy Charles <gardimycharles889@gmail.com>
2026-07-24 11:04:40 -0400

    Update main.ts file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,6 +11,10 @@ import {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin:"http://localhost:5173",
+});
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
